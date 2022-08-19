15a6709d357a3f523ab536e2dedd74e51eda4732
zakir <zakir158166@gmail.com>
2022-08-19 21:57:30 +0600

    if and switch statement updated code

--- src/test/java/Main.java
+++ src/test/java/Main.java
@@ -2,46 +2,70 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner userInput = new Scanner (System.in);
+
+        // Use If & switch statement to check the number is positive,negative or Zero by taking input from user.
+        Scanner userInput = new Scanner(System.in);
         System.out.print("Please input require number:");
         int num = userInput.nextInt();
-        if (num>0){
-            System.out.println("User input: '"+num+"' is Positive Value");
-        }
-        else if (num<0) {
-            System.out.println("User input: '"+num+"' is Negative Value");
+        int result =switchMethod(num);
 
-        }
-        else {
-            System.out.println("User input: '"+num+"' is Zero");
-
+        if (num > 0) {
+            System.out.println("User input: '" + num + "' is Positive Value");
+        } else if (num < 0) {
+            System.out.println("User input: '" + num + "' is Negative Value");
+        } else {
+            System.out.println("User input: '" + num + "' is Zero");
         }
 
-        int num1=10;
-        if (num1>0){
-            System.out.println("Number("+num1+") contain Positive Value");
-        }else if (num1<0){
-            System.out.println("Number("+num1+") contain Negative Value");
-        }else {
-            System.out.println("Number("+num1+") contain equal Value");
+        switch (result){
+            case 1:
+                System.out.println("switch statement "+num+" is positive");
+                break;
+            case -1:
+                System.out.println("switch statement "+num+" is Negative");
+                break;
+            default:
+                System.out.println("switch statement "+num+" is Zero");
         }
 
-        class SwitchClass {
-        public static int positive (int inp){
-            if (inp>0)
-            return 1;
-            else if (inp<0)
-            return -1;
-            else
-            return 1;
+        // Use If & switch statement to check the number is positive,negative or Zero by taking value from variable.
+        int num1 = 1;
 
-//            switch (positive(inp)){
-//                case 1:
-//                    System.out.println("positive");
-//            }
+        if (num1 > 0) {
+            System.out.println("Number(" + num1 + ") contain Positive Value");
+        } else if (num1 < 0) {
+            System.out.println("Number(" + num1 + ") contain Negative Value");
+        } else {
+            System.out.println("Number(" + num1 + ") contain Zero Value");
         }
+        switch (num1){
+            case 1:
+                System.out.println("Positive");
+                break;
+            case -1:
+                System.out.println("Negative");
+                break;
+            case 0:
+                System.out.println("Zero");
+                break;
+            default:
+                System.out.println("Not match");
         }
+
+    }
+    //Using a new method to identify the match case which is used in switch statement
+    public static int switchMethod(int num) {
+            if (num > 0) {
+                return 1;
+            } else if (num < 0) {
+                return -1;
+            } else {
+                return 0;
+            }
+
+
+    }
     }
 
 
-}
+
