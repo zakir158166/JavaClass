864de97a93fb173115ce4734ebbe9ca1603390a4
zakir <zakir158166@gmail.com>
2022-08-19 18:17:06 +0600

    first commit in Java Class

---
+++ src/test/java/New.java
@@ -0,0 +1,7 @@
+public class New {
+    public static void main(String[] args) {
+        String name="zakir";
+        System.out.println("this is a new class of Java Language");
+        System.out.println(name);
+    }
+}
