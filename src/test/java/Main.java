5653957648839bb1c1c1c90fc959e74a9060ca36
zakir <zakir158166@gmail.com>
2022-08-19 22:21:34 +0600

    even and odd code updated

--- src/test/java/Main.java
+++ src/test/java/Main.java
@@ -7,7 +7,7 @@ public class Main {
         Scanner userInput = new Scanner(System.in);
         System.out.print("Please input require number:");
         int num = userInput.nextInt();
-        int result =switchMethod(num);
+        int result = switchMethod(num);
 
         if (num > 0) {
             System.out.println("User input: '" + num + "' is Positive Value");
@@ -17,16 +17,22 @@ public class Main {
             System.out.println("User input: '" + num + "' is Zero");
         }
 
-        switch (result){
+        switch (result) {
             case 1:
-                System.out.println("switch statement "+num+" is positive");
+                System.out.println("switch statement " + num + " is positive");
                 break;
             case -1:
-                System.out.println("switch statement "+num+" is Negative");
+                System.out.println("switch statement " + num + " is Negative");
                 break;
             default:
-                System.out.println("switch statement "+num+" is Zero");
+                System.out.println("switch statement " + num + " is Zero");
         }
+
+        // check even or odd from user input
+        if (num % 5 == 0) {
+            System.out.println("The number is Even");
+        } else {
+            System.out.println("The number is Odd");}
 
         // Use If & switch statement to check the number is positive,negative or Zero by taking value from variable.
         int num1 = 1;
@@ -38,7 +44,7 @@ public class Main {
         } else {
             System.out.println("Number(" + num1 + ") contain Zero Value");
         }
-        switch (num1){
+        switch (num1) {
             case 1:
                 System.out.println("Positive");
                 break;
@@ -52,20 +58,32 @@ public class Main {
                 System.out.println("Not match");
         }
 
-    }
+        // check even or odd from variable value
+
+        if (num1 - 1 ==0) {
+            System.out.println("It is even");
+        }
+            else {
+                System.out.println("It is Odd");
+            }
+        }
+
+
+
     //Using a new method to identify the match case which is used in switch statement
     public static int switchMethod(int num) {
-            if (num > 0) {
-                return 1;
-            } else if (num < 0) {
-                return -1;
-            } else {
-                return 0;
-            }
+        if (num > 0) {
+            return 1;
+        } else if (num < 0) {
+            return -1;
+        } else {
+            return 0;
+        }
 
 
     }
-    }
+
+}
 
 
 
